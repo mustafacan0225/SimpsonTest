f719e5bec254a8557d0e1eb6e616542f65d9b24e
orananaliznet <orananaliznet@gmail.com>
2020-05-03 16:37:51 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by mustafacan on 3.05.2020.
+//  Copyright © 2020 mustafacan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
